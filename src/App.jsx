7758e27b84950b987388c38a45dbf75eb0fea0f3
Joe Bailey <joebailey1000@hotmail.co.uk>
2023-10-24 11:16:29 +0100

    can show a list of topics, and when one is selected shows all the articles from that topic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Header } from './Header'
 import { ArticleCards } from './ArticleCards'
 import { Routes, Route, Link } from 'react-router-dom'
 import { SingleArticle } from './SingleArticle'
+import { TopicMenu } from './TopicMenu'
+import { TopicCards } from './TopicCards'
+
 
 function App() {
   const [loggedInUser,setLoggedInUser]=useState('grumpy19')
@@ -11,9 +14,12 @@ function App() {
     <>
       <Header />
       <Link to='articles'>to articles</Link>
+      <Link to='topics'>to topics</Link>
       <Routes>
-        <Route path='/articles' element={<ArticleCards showBody={false}/>} />
+        <Route path='/articles' element={<ArticleCards/>} />
         <Route path='/articles/:article_id' element={<SingleArticle loggedInUser={loggedInUser}/>} />
+        <Route path='/topics' element={<TopicMenu/>} />
+        <Route path='/topics/:slug' element={<TopicCards/>}/>
       </Routes>
     </>
   )
